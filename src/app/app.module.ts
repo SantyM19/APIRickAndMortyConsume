50251d44894c2ea2b4fab7ffa0ad83716f88750d
santiagomejia <santiagomejiad19@hotmail.com>
2022-07-31 16:15:14 -0500

    BackUp App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { RickAndMortyComponent } from './pages/rick-and-morty/rick-and-morty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CharactersComponent,
+    RickAndMortyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/rick-and-morty/rick-and-morty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rick-and-morty',
+  templateUrl: './rick-and-morty.component.html',
+  styleUrls: ['./rick-and-morty.component.css']
+})
+export class RickAndMortyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
